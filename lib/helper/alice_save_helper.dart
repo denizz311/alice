351fe62c64e0868df1ec1a2f9e0d94eb8f918581
dev <denys.loziuk@uapay.ua>
2020-10-08 10:41:34 +0300

    bank2: file format json

--- lib/helper/alice_save_helper.dart
+++ lib/helper/alice_save_helper.dart
@@ -62,7 +62,7 @@ class AliceSaveHelper {
           ? getExternalStorageDirectory()
           : getApplicationDocumentsDirectory());
       String fileName =
-          "alice_log_${DateTime.now().millisecondsSinceEpoch}.txt";
+          "alice_log_${DateTime.now().millisecondsSinceEpoch}.json";
       File file = File(externalDir.path.toString() + "/" + fileName);
       file.createSync();
       IOSink sink = file.openWrite(mode: FileMode.append);
@@ -104,15 +104,15 @@ class AliceSaveHelper {
     // stringBuffer.write("Alice - HTTP Inspector\n");
     map['title'] = "Alice - HTTP Inspector";
     // stringBuffer.write("App name:  ${packageInfo.appName}\n");
-    map['app_name'] = "${packageInfo.appName}".toString();
+    map['app_name'] = packageInfo.appName;
     // stringBuffer.write("Package: ${packageInfo.packageName}\n");
-    map['package_name'] = "${packageInfo.packageName}".toString();
+    map['package_name'] = packageInfo.packageName;
     // stringBuffer.write("Version: ${packageInfo.version}\n");
-    map['version'] = "${packageInfo.version}".toString();
+    map['version'] = packageInfo.version;
     // stringBuffer.write("Build number: ${packageInfo.buildNumber}\n");
-    map['build_number'] = "${packageInfo.buildNumber}".toString();
+    map['build_number'] = packageInfo.buildNumber;
     // stringBuffer.write("Generated: " + DateTime.now().toIso8601String() + "\n");
-    map['createdAt'] = "${DateTime.now().toIso8601String()}".toString();
+    map['createdAt'] = DateTime.now().toIso8601String();
     // stringBuffer.write("\n");
     return map;
   }
